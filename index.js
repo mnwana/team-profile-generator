7d0748a6b4a9b1f54d824b6cd0e37e3efc18281f
mnwana <mariellenwana@gmail.com>
2022-08-14 21:31:45 -0400

    add prompts for employee types:

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const inquirer = require("inquirer");
 const { Employee, Intern, Manager, Engineer } = require("./lib/classes");
 const generateHtml = require("./lib/generateHTML");
 
+const employees = {
+  managers: [],
+  engineers: [],
+  interns: [],
+};
+
 const promptUser = function (employeeArr) {
   if (!employeeArr) {
     employeeArr = [];
@@ -72,7 +78,7 @@ const promptUser = function (employeeArr) {
       },
     ])
     .then((employeeData) => {
-      employeeArr.push(constructEmployee(employeeData));
+      employeeArr.push(constructEmployee(employeeData,employeeData.role));
       if (employeeData.confirmAdd) {
         return promptUser(employeeArr);
       } else {
@@ -81,12 +87,173 @@ const promptUser = function (employeeArr) {
     });
 };
 
-const constructEmployee = function (empObj) {
-  if (empObj.role == "Manager") {
+const promptManager = function (employeeArr) {
+    return inquirer
+      .prompt([
+        {
+          name: "name",
+          message: "What is this Managers's name?",
+          type: "input",
+          validate: (nameInput) => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log("Please enter a name.");
+              return false;
+            }
+          },
+        },
+        {
+          name: "id",
+          type: "input",
+          message: "What is this manager's ID?",
+        },
+        {
+          name: "email",
+          type: "input",
+          message: "What is this manager's email address?",
+        },
+        {
+          name: "office",
+          type: "input",
+          message: "What is this Manager's office number?",
+          when: ({ role }) => {
+            return role == "Manager";
+          },
+        },
+  
+        {
+          name: "confirmAdd",
+          message: "Would you like to add another Manager?",
+          type: "confirm",
+        },
+      ])
+      .then((employeeData) => {
+        employeeArr.push(constructEmployee(employeeData,'Manager'));
+        if (employeeData.confirmAdd) {
+          return promptUser(employeeArr);
+        } else {
+          return employeeArr;
+        }
+      });
+  };
+
+  const promptEngineer = function (employeeArr) {
+    if (!employeeArr) {
+      employeeArr = [];
+    }
+    return inquirer
+      .prompt([
+        {
+          name: "name",
+          message: "What is the engineers's name?",
+          type: "input",
+          validate: (nameInput) => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log("Please enter a name.");
+              return false;
+            }
+          },
+        },
+        {
+          name: "id",
+          type: "input",
+          message: "What is this engineer's ID?",
+        },
+        {
+          name: "email",
+          type: "input",
+          message: "What is this engineer's email address?",
+        },
+  
+        {
+          name: "github",
+          type: "input",
+          message: "What is this Engineer's GitHub username?",
+          when: ({ role }) => {
+            return role == "Engineer";
+          },
+        },
+        {
+          name: "confirmAdd",
+          message: "Would you like to add another engineer?",
+          type: "confirm",
+        },
+      ])
+      .then((employeeData) => {
+        employeeArr.push(constructEmployee(employeeData,'Engineer'));
+        if (employeeData.confirmAdd) {
+          return promptUser(employeeArr);
+        } else {
+          return employeeArr;
+        }
+      });
+  };
+
+  const promptIntern = function (employeeArr) {
+    if (!employeeArr) {
+      employeeArr = [];
+    }
+    return inquirer
+      .prompt([
+        {
+          name: "name",
+          message: "What is this intern's name?",
+          type: "input",
+          validate: (nameInput) => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log("Please enter a name.");
+              return false;
+            }
+          },
+        },
+        {
+          name: "id",
+          type: "input",
+          message: "What is this intern's ID?",
+        },
+        {
+          name: "email",
+          type: "input",
+          message: "What is this intern's email address?",
+        },
+       
+  
+        {
+          name: "school",
+          type: "input",
+          message: "What is this intern's school name?",
+          when: ({ role }) => {
+            return role == "Intern";
+          },
+        },
+  
+        {
+          name: "confirmAdd",
+          message: "Would you like to add another intern?",
+          type: "confirm",
+        },
+      ])
+      .then((employeeData) => {
+        employeeArr.push(constructEmployee(employeeData, 'Intern'));
+        if (employeeData.confirmAdd) {
+          return promptUser(employeeArr);
+        } else {
+          return employeeArr;
+        }
+      });
+  };
+
+const constructEmployee = function (empObj,role) {
+  if (role == "Manager") {
     return new Manager(empObj.name, empObj.id, empObj.email, empObj.office);
-  } else if (empObj.role == "Engineer") {
+  } else if (role == "Engineer") {
     return new Engineer(empObj.name, empObj.id, empObj.email, empObj.github);
-  } else if (empObj.role == "Intern") {
+  } else if (role == "Intern") {
     return new Intern(empObj.name, empObj.id, empObj.email, empObj.school);
   } else {
     return new Employee(empObj.name, empObj.id, empObj.email);
